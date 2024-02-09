8466b9066c6c42ce291617bb61fbb3d24cebe64a
Maria G <mgorban@rheinahrcampus.de>
2024-02-09 17:49:51 +0100

    add some last corrections

--- src/Main.java
+++ src/Main.java
@@ -29,40 +29,40 @@ public class Main {
   public static void main(String[] args) {
     History transactions = new History(); // объект класса History, который будет хранить в себе историю трансакций
     Scanner scanner = new Scanner(System.in);
-    Exchanger exchangeRates = new Exchanger(); // создаем базу данных, которая будет хранить в себе обменные курсы
+    Exchanger exchangeRate = new Exchanger(); // создаем базу данных, которая будет хранить в себе обменные курсы
 
     // Добавление курсов обмена для разных валют в базу
-    exchangeRates.addExchangeRate(Currency.USD.getName(), Currency.EUR.getName(), 0.93);
-    exchangeRates.addExchangeRate(Currency.USD.getName(), Currency.JPY.getName(), 149.30);
-    exchangeRates.addExchangeRate(Currency.USD.getName(), Currency.GBP.getName(), 0.79);
-    exchangeRates.addExchangeRate(Currency.USD.getName(), Currency.RUB.getName(), 91.18);
+    exchangeRate.addExchangeRate(Currency.USD.getName(), Currency.EUR.getName(), 0.93);
+    exchangeRate.addExchangeRate(Currency.USD.getName(), Currency.JPY.getName(), 149.30);
+    exchangeRate.addExchangeRate(Currency.USD.getName(), Currency.GBP.getName(), 0.79);
+    exchangeRate.addExchangeRate(Currency.USD.getName(), Currency.RUB.getName(), 91.18);
 
-    exchangeRates.addExchangeRate(Currency.EUR.getName(), Currency.USD.getName(), 1.08);
-    exchangeRates.addExchangeRate(Currency.EUR.getName(), Currency.JPY.getName(), 160.93);
-    exchangeRates.addExchangeRate(Currency.EUR.getName(), Currency.GBP.getName(), 0.85);
-    exchangeRates.addExchangeRate(Currency.EUR.getName(), Currency.RUB.getName(), 98.28);
+    exchangeRate.addExchangeRate(Currency.EUR.getName(), Currency.USD.getName(), 1.08);
+    exchangeRate.addExchangeRate(Currency.EUR.getName(), Currency.JPY.getName(), 160.93);
+    exchangeRate.addExchangeRate(Currency.EUR.getName(), Currency.GBP.getName(), 0.85);
+    exchangeRate.addExchangeRate(Currency.EUR.getName(), Currency.RUB.getName(), 98.28);
 
-    exchangeRates.addExchangeRate(Currency.JPY.getName(), Currency.USD.getName(), 0.0067);
-    exchangeRates.addExchangeRate(Currency.JPY.getName(), Currency.EUR.getName(), 0.0062);
-    exchangeRates.addExchangeRate(Currency.JPY.getName(), Currency.GBP.getName(), 0.0053);
-    exchangeRates.addExchangeRate(Currency.JPY.getName(), Currency.RUB.getName(), 0.61);
+    exchangeRate.addExchangeRate(Currency.JPY.getName(), Currency.USD.getName(), 0.0067);
+    exchangeRate.addExchangeRate(Currency.JPY.getName(), Currency.EUR.getName(), 0.0062);
+    exchangeRate.addExchangeRate(Currency.JPY.getName(), Currency.GBP.getName(), 0.0053);
+    exchangeRate.addExchangeRate(Currency.JPY.getName(), Currency.RUB.getName(), 0.61);
 
-    exchangeRates.addExchangeRate(Currency.GBP.getName(), Currency.USD.getName(), 1.26);
-    exchangeRates.addExchangeRate(Currency.GBP.getName(), Currency.JPY.getName(), 188.36);
-    exchangeRates.addExchangeRate(Currency.GBP.getName(), Currency.EUR.getName(), 1.17);
-    exchangeRates.addExchangeRate(Currency.GBP.getName(), Currency.RUB.getName(), 115.04);
+    exchangeRate.addExchangeRate(Currency.GBP.getName(), Currency.USD.getName(), 1.26);
+    exchangeRate.addExchangeRate(Currency.GBP.getName(), Currency.JPY.getName(), 188.36);
+    exchangeRate.addExchangeRate(Currency.GBP.getName(), Currency.EUR.getName(), 1.17);
+    exchangeRate.addExchangeRate(Currency.GBP.getName(), Currency.RUB.getName(), 115.04);
 
-    exchangeRates.addExchangeRate(Currency.RUB.getName(), Currency.USD.getName(), 0.011);
-    exchangeRates.addExchangeRate(Currency.RUB.getName(), Currency.JPY.getName(), 1.64);
-    exchangeRates.addExchangeRate(Currency.RUB.getName(), Currency.GBP.getName(), 0.0087);
-    exchangeRates.addExchangeRate(Currency.RUB.getName(), Currency.EUR.getName(), 0.010);
+    exchangeRate.addExchangeRate(Currency.RUB.getName(), Currency.USD.getName(), 0.011);
+    exchangeRate.addExchangeRate(Currency.RUB.getName(), Currency.JPY.getName(), 1.64);
+    exchangeRate.addExchangeRate(Currency.RUB.getName(), Currency.GBP.getName(), 0.0087);
+    exchangeRate.addExchangeRate(Currency.RUB.getName(), Currency.EUR.getName(), 0.010);
 
     printCurrencies(); // вывод валют
 
     int choice;
     do {
       System.out.println(
-          "\nWould you like to make a transaction (1), view your history (2) or finish any transaction (0)? ");
+          "\nWould you like to make a transaction (1), see your history (2) or finish any transaction (0)? ");
       choice = scanner.nextInt();
 
       switch (choice) {
@@ -73,7 +73,7 @@ public class Main {
           scanner.nextLine();
           Currency trgCurr = askAboutCurrency("Enter the currency you want to have: ");
 
-          String deal = exchangeRates.convertCurrency(amount, srcCurr.getName(), trgCurr.getName());
+          String deal = exchangeRate.convertCurrency(amount, srcCurr.getName(), trgCurr.getName());
           System.out.println("\n" + deal);
           transactions.addTransaction(srcCurr.getName(), trgCurr.getName(), amount);
           break;
